1bcb0a2591afd5cbe7f7f0308400924d73664317
KennieJ <kennie.jfive@gmail.com>
2020-11-21 17:23:42 -0500

    2nd Commit

--- 06_RepositoryPattern_Console/ProgramUI.cs
+++ 06_RepositoryPattern_Console/ProgramUI.cs
@@ -9,9 +9,12 @@ namespace _06_RepositoryPattern_Console
 {
     class ProgramUI
     {
+        private StreamingContentRepository _contentRepo = new StreamingContentRepository();
+
         // method that runs/starts the application
         public void Run()
         {
+            SeedContentList();
             Menu();
         }
 
@@ -77,12 +80,7 @@ namespace _06_RepositoryPattern_Console
         //Create new StreamingContent
         private void CreateNewContent()
         {
-
-        }
-
-        //View current StreamingContent that is saved
-        private void DisplayAllContent()
-        {
+            Console.Clear();
             StreamingContent newContent = new StreamingContent();
 
             //Title
@@ -105,8 +103,8 @@ namespace _06_RepositoryPattern_Console
             //Is family friendly
             Console.WriteLine("Is this content family friendly? (y/n)");
             string familyFriendlyString = Console.ReadLine().ToLower();
-            
-            if(familyFriendlyString == "y")
+
+            if (familyFriendlyString == "y")
             {
                 newContent.IsFamilyFriendly = true;
             }
@@ -123,36 +121,169 @@ namespace _06_RepositoryPattern_Console
                 "4. Documentary\n" +
                 "5. Bromance\n" +
                 "6. Drama\n" +
-                "7. Action\n");
-            
-            /*
-                Horror = 1,
-                RomCom,
-                SciFi,
-                Documentary,
-                Bromance,
-                Drama,
-                Action 
-             */
+                "7. Action");
 
+            string genreAsString = Console.ReadLine();
+            int genreAsInt = int.Parse(genreAsString);
+            newContent.TypeOfGenre = (GenreType)genreAsInt; //casting
+
+            _contentRepo.AddContentToList(newContent);
+        }
+
+        //View current StreamingContent that is saved
+        private void DisplayAllContent()
+        {
+            Console.Clear(); //gets rid of menu options after choice is made
+            
+            List<StreamingContent> listOfContent = _contentRepo.GetContentList();
+
+            foreach(StreamingContent content in listOfContent)
+            {
+                Console.WriteLine($"Title: {content.Title}\n" +
+                    $"Description: {content.Description}");
+            }
         }
 
         //View existing Content by title
         private void DisplayContentByTitle()
         {
+            Console.Clear();
+            //Prompt user to give a title
+            Console.WriteLine("Enter the title of the content you'd like to see:");
 
+            //get the users input
+            string title = Console.ReadLine();
+
+            // Find the content by the title given
+            StreamingContent content = _contentRepo.GetContentByTitle(title);
+
+            //Display the content if it isn't null
+            if (content != null)
+            {
+                Console.WriteLine($"Title: {content.Title}\n" +
+                    $"Description: {content.Description}\n" +
+                    $"Maturity Rating: {content.MaturityRating}\n" +
+                    $"Stars: {content.StarRating}\n" +
+                    $"Family Friendly?: {content.IsFamilyFriendly}\n" +
+                    $"Genre: {content.TypeOfGenre}");
+            }
+            else
+            {
+                Console.WriteLine("No content by that title.");
+            }
         }
 
         //Update Existing content
         private void UpdateExistingContent()
         {
+            //Display all content
+            DisplayAllContent();
+
+            //Ask for the title of content to be updated
+            Console.WriteLine("Enter the title of the content you'd like to update:");
+
+            //Get that title
+            string oldTitle = Console.ReadLine();
+
+            //build a new object
+            Console.Clear();
+            StreamingContent newContent = new StreamingContent();
+
+            //Title
+            Console.WriteLine("Enter the title for the content:");
+            newContent.Title = Console.ReadLine();
+
+            //Description
+            Console.WriteLine("Enter the description for the content:");
+            newContent.Description = Console.ReadLine();
+
+            //Maturity Rating
+            Console.WriteLine("Enter the rating for the content (G, PG, PG-13, etc):");
+            newContent.MaturityRating = Console.ReadLine();
+
+            //Star Rating
+            Console.WriteLine("Enter the star count for the content (5.8, 10, 1.5, etc.):");
+            string starsAsString = Console.ReadLine();
+            newContent.StarRating = double.Parse(starsAsString);
+
+            //Is family friendly
+            Console.WriteLine("Is this content family friendly? (y/n)");
+            string familyFriendlyString = Console.ReadLine().ToLower();
+
+            if (familyFriendlyString == "y")
+            {
+                newContent.IsFamilyFriendly = true;
+            }
+            else
+            {
+                newContent.IsFamilyFriendly = false;
+            }
+
+            //GenreType
+            Console.WriteLine("Enter the Genre Number:\n" +
+                "1. Horror\n" +
+                "2. RomCom\n" +
+                "3. SciFi\n" +
+                "4. Documentary\n" +
+                "5. Bromance\n" +
+                "6. Drama\n" +
+                "7. Action");
+
+            string genreAsString = Console.ReadLine();
+            int genreAsInt = int.Parse(genreAsString);
+            newContent.TypeOfGenre = (GenreType)genreAsInt;
+
+            //Verify the update worked
+            bool wasUpdated = _contentRepo.UpdateExistingContent(oldTitle, newContent);
+
+            if (wasUpdated)
+            {
+                Console.WriteLine("Content successfully updated");
+            }
+            else
+            {
+                Console.WriteLine("Could not update content");
+            }
 
         }
 
         //Delete Existing Content
         private void DeleteExistingContent()
         {
+            DisplayAllContent();
 
+            //Get the title they want to remove
+            Console.WriteLine("\nEnter the title you'd like to remove:");
+
+            string input = Console.ReadLine();
+
+            //Call the delete method
+            bool wasDeleted = _contentRepo.RemoveContentFromList(input);
+
+            //If the content was deleted, say so
+            //Otherwise state it could not be deleted
+            if (wasDeleted)
+            {
+                Console.WriteLine("The content was successfully deleted.");
+            }
+            else
+            {
+                Console.WriteLine("The content could not be deleted.");
+            }
+        }
+
+        //See method
+        private void SeedContentList()
+        {
+            StreamingContent sharknado = new StreamingContent("Sharknado", "Tornadoes, but make it sharks", "TV-14", 3.3, true, GenreType.Action);
+            StreamingContent theRoom = new StreamingContent("The Room", "Banker's life gets flipped, turned upside down", "R", 3.7, false, 
+                GenreType.Drama);
+            StreamingContent rubber = new StreamingContent("Rubber", "Car tire comes to life and Tokyo Drifts you to hell", "R", 5.8, false, 
+                GenreType.Documentary);
+
+            _contentRepo.AddContentToList(sharknado);
+            _contentRepo.AddContentToList(theRoom);
+            _contentRepo.AddContentToList(rubber);
         }
     }
 }
